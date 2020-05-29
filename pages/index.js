0477db969876b548b746587cea4367fba5d37cf1
Janusz Janczy <janusz.janczy@snow.dog>
2020-05-29 13:40:55 -0300

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+const Title = styled.h1`
+  font-size: 40px;
+  color: darkorange;
+  font-family: 'Nunito', sans-serif;
+`
+
+export default () => <Title>Hello, Herm</Title>
